742e558e5736fef7ea04d8233cbb7a965963c476
Abstractn <58334120+Abstractn@users.noreply.github.com>
2020-01-31 12:39:07 +0100

    save-load system

--- res/script/funcs.js
+++ res/script/funcs.js
@@ -45,7 +45,6 @@ function killEnemy(enemy)
 function spawnRandomEnemy()
 {
 	var i = getRandomInt(0,enemyPool.length-1);
-	//gameState.enemy.setTexture( PIXI.TextureCache[key] );
 	sprite_url = "res/imgs/enemy/"+enemyPool[i]+".png";
 	document.getElementById("enemy").src = sprite_url;
 	log("(rand)enemy_pool="+i);
@@ -55,11 +54,21 @@ function spawnRandomEnemy()
 function setCurrency(value)
 { player_currency = value; return "u cheaty biscuit"; }
 
-function player_stats_getFullDamage()
-{ return player_stats_baseDamage + player_stats_bonusDamage; }
-
 function loadGame()
 {
+	var data = prompt("Please insert your data string");
+	data = data.split("|");
+	for(var i=0; i<data.length;i++)
+	{ data[i] = data[i] *1; } // converts from string to number
+
+	player.stats.baseDmg = data[0];
+	player.stats.bonusDmg = data[1];
+	player.currency = data[2];
+	enemy.maxHP = data[3];
+
+	// auto close menu
+	showMenu();
+	forceRedraw("show_enemyHP");
 }
 
 function saveGame()
@@ -68,6 +77,8 @@ function saveGame()
 	           player.stats.bonusDmg + "|" +
 			   player.currency + "|" +
 			   enemy.maxHP;
+	               
+	alert("Please copy and store the following string:\n" + data);
 	console.log("saveGame();data{"+data+"}");
 }
 
@@ -81,3 +92,15 @@ function showMenu()
 	else
 	{ document.getElementById("menu_box").style.display = "none"; }
 }
+
+var prevState;
+	
+function forceRedraw(element)
+{
+    //prevState = document.getElementById(element).style.display;
+    document.getElementById(element).style.display = "none";
+    document.getElementById(element).style.display = "block";
+    //log("forced redraw of " + element + "; state: " +prevState);
+}
+
+// document.getElementById("show_enemyHP").style.display
