d9fe07f197a87487727a37795ef6587c63a63835
Abstractn <58334120+Abstractn@users.noreply.github.com>
2020-02-03 10:43:10 +0100

    stage counting

--- res/script/main.js
+++ res/script/main.js
@@ -1,6 +1,6 @@
 document.getElementById("play_screen").style.width = screen.w;
 document.getElementById("play_screen").style.height = screen.h;
 
-spawnRandomEnemy();
+enemy.spawn("random");
 
 document.getElementById("show_enemyHP").innerHTML = enemy.maxHP;
